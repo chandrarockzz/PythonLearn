7a09d0283ca98f18899335e4a0dc0d0c93de144d
chandrarockzz <sekhar.916@gmail.com>
2018-06-08 15:23:28 +0530

    Added new learnings

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,8 @@
 import this
 from _ast import Add
 from locale import str
+import platform
+import datetime
 
 print('HelloWorld')
 a = 10
@@ -46,7 +48,8 @@ else:
 
 print(a.count("Hello", 0, 17))
 
-#Functions-->>
+
+# Functions-->>
 def my_function():
     print('hai I am in FUnction')
 
@@ -81,7 +84,6 @@ thislist[1] = "Hai"
 print(thislist)
 
 
-
 def SwapNumber(a, b):
     a = a + b
     b = a - b
@@ -98,8 +100,8 @@ def Multiply(a, b):
 
 
 print(Multiply(10, 20))
-#Lists ------>>
-#A list is a collection which is ordered and changeable. In Python lists are written with square brackets.
+# Lists ------>>
+# A list is a collection which is ordered and changeable. In Python lists are written with square brackets.
 thislist = ["Applle", "Cherry", "Oranges"]
 print(thislist)
 print(thislist[0])
@@ -120,16 +122,16 @@ testlist.remove("Cherry")
 print(testlist)
 print(len(testlist))
 
-#Tuple----->
-#A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
+# Tuple----->
+# A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
 thistuple = ("Hello", "World", "Hai")
 print(thistuple)
 print(thistuple.count("Hello"))
-#thistuple[0] = "Hell" this is an error as tuple cant be changed
+# thistuple[0] = "Hell" this is an error as tuple cant be changed
 print(len(thistuple))
 
-#Set
-#A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+# Set
+# A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
 thisset = {"Hai", "Hello", "World"}
 print(thisset)
 print(len(thisset))
@@ -138,13 +140,13 @@ print(thisset)
 thisset.add("How")
 print(thisset)
 
-#Dictionary
-#A dictionary is a collection which is unordered, changeable and indexed.
+# Dictionary
+# A dictionary is a collection which is unordered, changeable and indexed.
 # In Python dictionaries are written with curly brackets, and they have keys and values.
 thisdict = {
-    "Apple" : "Green",
-    "Banana" : "yellow",
-    "Mango"  : "yellow"
+    "Apple": "Green",
+    "Banana": "yellow",
+    "Mango": "yellow"
 }
 
 print(thisdict)
@@ -153,7 +155,7 @@ print(thisdict)
 thisdict["Cherry"] = "red"
 print(len(thisdict))
 print(thisdict)
-del(thisdict["Apple"])
+del (thisdict["Apple"])
 print(len(thisdict))
 print(thisdict)
 
@@ -166,19 +168,83 @@ while i < 6:
     print(i)
     if i == 4:
         break
-    i = i+1
+    i = i + 1
 
 i = 0
 while i < 6:
-    i = i+1
+    i = i + 1
     if i == 4:
         continue
     print(i)
 
 i = 0
 while i < 100:
-    i = i+1
+    i = i + 1
     if i == 50:
         continue
     print(i)
 
+fruits = ["Apple", "Orange", "CHerry"]
+for x in fruits:
+    print(x)
+for x in range(6):
+    print(x)
+
+for x in a:
+    print(x)
+for x in range(len(a)):
+    print(a[x])
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+#Lambda Functions
+#In python, the keyword lambda is used to create what is known as anonymous functions.
+# These are essentially functions with no pre-defined name. They are good for constructing adaptable functions,
+# and thus good for event handling.
+myfunc = lambda i: i*2
+print(myfunc(2))
+myfunc = lambda x,y: x*y
+print(myfunc(3,6))
+
+def myfunc(n):
+  return lambda i: i*n
+
+doubler = myfunc(2)
+tripler = myfunc(3)
+val = 11
+print("Doubled: " + str(doubler(val)) + ". Tripled: " + str(tripler(val)))
+
+class MyClass:
+    x = 5
+p1 = MyClass()
+print(p1.x)
+
+class MyDetails:
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+
+p1 = MyDetails("Ramesh", 20)
+print(p1.name)
+print(p1.age)
+
+class Test:
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+    def Name(self):
+        print("My Name is "+ self.name)
+p1 = Test("Chandra", 29)
+p1.Name()
+
+x = dir(platform)
+print(x)
+
+x = datetime.datetime.now()
+print(x)
